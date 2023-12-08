206494d769ac2688e7463439a36be78af5ef1220
ehurafa <rafarfl2010@gmail.com>
2023-12-08 19:41:33 -0400

    (template)  Creting the components

---
+++ portfolio/src/components/Thumb.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Thumb = () => {
+  return (
+    <div className="thumb">
+      <img src="" alt="" />
+      <h3>Thumb</h3>
+    </div>
+  )
+}
+
+export default Thumb
